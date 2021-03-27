0483ed264af9034e8dd389ed335baa0a41cc574c
myname <i68s2j8rxy@i.softbank.jp>
2021-03-28 04:07:19 +0900

    87th commit

--- src/plugins/materials/atoms/Movie-icon.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div id="movie-icon">
-    <img src="../assets/movie.png" width="25px" height="25px" id="icon" />
-  </div>
-</template>
--- src/plugins/materials/atoms/Sort-icon.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div id="sort-icon">
-    <img src="../assets/sort.png" width="30px" height="30px" id="icon3" />
-  </div>
-</template>
